a6241a6f451d928796f460276b0d6fd6df3b56da
Asya <nastya.Z1998@yandex.ru>
2022-02-17 13:27:12 +0500

    calc final

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
 def sum(update: Update, context: CallbackContext):
     msg = update.message.text
     update.message.reply_text(f'{eval(msg)}')
