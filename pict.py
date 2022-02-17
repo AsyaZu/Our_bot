acceb99dabf0f9e23714c379c63707e3ea24b3bf
Marina F <fedma001@mail.ru>
2022-02-17 11:32:13 +0300

    made graph with our data

--- pict.py
+++ pict.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def pict(update: Update, context: CallbackContext):
-    x = np.arange(0, 10, 0.1)
+    msg = update.message.text
+    items = msg.split()# сплитим сообщение
+    a = int(items[1])
+    b = int(items[2])
+    c = int(items[3])
+    x = np.arange(a, b, c)
     y = np.sin(x)
 
     plt.plot(x, y)
     plt.savefig('saved_figure.png') #сохраняем график в файл
     #plt.show()
 
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    update.message.reply_text(f'sin {a}, {b}, {c}')
 
 
 
